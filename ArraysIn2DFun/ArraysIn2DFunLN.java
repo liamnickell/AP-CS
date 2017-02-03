bf82a7787e08825496b770372e29b6c5c55d8a40
Liam Nickell <liam.nickell25@gmail.com>
2017-02-03 15:08:51 -0600

    modified to conform to assignment

--- ArraysIn2DFun/ArraysIn2DFunLN.java
+++ ArraysIn2DFun/ArraysIn2DFunLN.java
@@ -1,23 +1,57 @@
 public class ArraysIn2DFunLN {
 
     public static void main(String[] args) {
+    
         int[][] nums = new int[10][10];
 
+        // array as grid in row major order
         for(int row=0; row<nums.length; row++) {
-            for(int i=0; i<161; i++) { System.out.print("-"); }
-            System.out.println();
-            System.out.print("|\t");
-
             for(int col=0; col<nums[row].length; col++) {
-                nums[row][col] = (row+1) * (col+1);
-                System.out.print(nums[row][col] + "\t|\t");
+                nums[row][col] = (col + 1) + (row * 10);
+                System.out.print(nums[row][col] + "\t");
             }
 
             System.out.println();
         }
 
-        for(int i=0; i<161; i++) { System.out.print("-"); }
+        System.out.println("\n");
+
+        // array as grid in column major order
+        for(int row=0; row<nums.length; row++) {
+            for(int col=0; col<nums[row].length; col++) {
+                System.out.print(nums[col][row] + "\t");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
+        // diagonal from top right to bottom left of array
+        for(int i=0; i<nums.length; i++) {
+            System.out.print(nums[i][i] + "\t");
+        }
+
+        System.out.println("\n\n");
+
+        // diagonal from top right to bottom left of array
+        for(int i=0; i<nums.length; i++) {
+            System.out.print(nums[i][nums.length-1-i] + "\t");
+        }
+
+        System.out.println("\n\n");
+
+        // array backwards as grid
+        for(int row=nums.length-1; row>=0; row--) {
+            for(int col=nums[row].length-1; col>=0; col--) {
+                System.out.print(nums[row][col] + "\t");
+            }
+
+            System.out.println();
+        }
+
         System.out.println();
+
     }
 
 }
