1f91e5cea0da5a83b29f29423956112ea86f773a
Liam Nickell <liam.nickell25@gmail.com>
2017-02-05 19:04:20 -0600

    modified ArraysIn2DFun

--- ArraysIn2DFun/ArraysIn2DFunLN.java
+++ ArraysIn2DFun/ArraysIn2DFunLN.java
@@ -4,7 +4,7 @@ public class ArraysIn2DFunLN {
     
         int[][] nums = new int[10][10];
 
-        // array as grid in row major order
+        System.out.println("Row Major Order:");
         for(int row=0; row<nums.length; row++) {
             for(int col=0; col<nums[row].length; col++) {
                 nums[row][col] = (col + 1) + (row * 10);
@@ -14,9 +14,9 @@ public class ArraysIn2DFunLN {
             System.out.println();
         }
 
-        System.out.println("\n");
+        System.out.println();
 
-        // array as grid in column major order
+        System.out.println("Column Major Order:");
         for(int row=0; row<nums.length; row++) {
             for(int col=0; col<nums[row].length; col++) {
                 System.out.print(nums[col][row] + "\t");
@@ -25,23 +25,23 @@ public class ArraysIn2DFunLN {
             System.out.println();
         }
 
-        System.out.println("\n");
+        System.out.println();
 
-        // diagonal from top right to bottom left of array
+        System.out.println("Diagonal Numbers from Top Left to Bottom Right:");
         for(int i=0; i<nums.length; i++) {
             System.out.print(nums[i][i] + "\t");
         }
 
-        System.out.println("\n\n");
+        System.out.println("\n");
 
-        // diagonal from top right to bottom left of array
+        System.out.println("Diagonal Numbers from Top Right to Bottom Left:");
         for(int i=0; i<nums.length; i++) {
             System.out.print(nums[i][nums.length-1-i] + "\t");
         }
 
-        System.out.println("\n\n");
+        System.out.println("\n");
 
-        // array backwards as grid
+        System.out.println("Backwards:");
         for(int row=nums.length-1; row>=0; row--) {
             for(int col=nums[row].length-1; col>=0; col--) {
                 System.out.print(nums[row][col] + "\t");
