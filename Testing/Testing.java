511baa7662df41f40c2c63eb10b8fb943c431a22
Liam Nickell <liam.nickell25@gmail.com>
2017-02-07 10:15:25 -0600

    tested ArrayLists and For Each loops

--- Testing/Testing.java
+++ Testing/Testing.java
@@ -1,10 +1,20 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class Testing {
 
     public static void main(String args[]){
     
-        
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+
+        nums.add(4);
+        nums.add(29);
+        nums.add(13);
+        nums.add(9);
+        nums.add(34);
+
+        for(int num : nums) {
+            System.out.println(num);
+        }
 
     }
 
